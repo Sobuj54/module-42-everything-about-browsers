0304591763c34fdb7cfa3e77d47ee11c19f512a1
sobuj <sobujahmed124@gmail.com>
2023-03-22 23:39:31 +0600

    local storage

---
+++ js/local.js
@@ -0,0 +1,7 @@
+console.log(localStorage.getItem("hellow"));
+
+const setItem = () => {
+  localStorage.setItem("name", "sobuj");
+  //   also can be written as
+  //  window.localStorage.setItem("name", "sobuj");
+};

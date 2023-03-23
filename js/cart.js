aa0eea7e64a199c9b261352a273c36256eb00430
sobuj <sobujahmed124@gmail.com>
2023-03-23 19:33:01 +0600

    finished

--- js/cart.js
+++ js/cart.js
@@ -9,14 +9,14 @@ const addProduct = () => {
   addList(product, quantity);
   saveDataToLocalStorage(product, quantity);
 };
-
+// adding product to the list from local storage
 const addList = (product, quantity) => {
   const ul = document.getElementById("list");
   const li = document.createElement("li");
   li.innerText = `${product} : ${quantity}`;
   ul.appendChild(li);
 };
-
+// getting data from local storage
 const getDataFromLocalStorage = () => {
   let cart = {};
   const storedCart = localStorage.getItem("cart");
@@ -25,11 +25,23 @@ const getDataFromLocalStorage = () => {
   }
   return cart;
 };
-
+// saving data to the local storage
 const saveDataToLocalStorage = (product, quantity) => {
   const cart = getDataFromLocalStorage();
   cart[product] = quantity;
   const cartStringified = JSON.stringify(cart);
   localStorage.setItem("cart", cartStringified);
-  console.log(cartStringified);
+  //console.log(cartStringified);
 };
+// showing data from local storage
+const showDataFromLocalStorage = () => {
+  const savedData = getDataFromLocalStorage();
+  //console.log(savedData);
+  for (const product in savedData) {
+    const quantity = savedData[product];
+    //console.log(product, quantity);
+    addList(product, quantity);
+  }
+};
+
+showDataFromLocalStorage();
